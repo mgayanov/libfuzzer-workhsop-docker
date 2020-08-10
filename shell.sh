0ca0f7fcff88e3d8e5d8938e8f6441b3ba54e3d4
Марат Гаянов <m.gayanov@dsec.ru>
2020-08-10 16:08:18 +0300

    initial

--- shell.sh
+++ shell.sh
@@ -3,5 +3,5 @@
 docker run --rm -ti -v `pwd`/hello-world:/home/workshop/hello-world \
                     -v `pwd`/heartbleed:/home/workshop/heartbleed \
                     -v `pwd`/c-ares:/home/workshop/c-ares \
-                    -v `pwd`/structure-aware:/home/workshop/structure-aware \
+                    -v `pwd`/protobuf-mutator:/home/workshop/protobuf-mutator \
                     workshop
